7e626748468a9abe9cd0c7a7f271891244dada14
HungHoangDinh <hunghd20012003@gmail.com>
2025-05-26 21:02:31 +0700

    rm cache

--- backend/test.py
+++
@@ -1,3 +0,0 @@
-from src.rag.utils.database_managements import DatabaseManager
-data_manager = DatabaseManager()
-print(data_manager.collection.query(query_texts=["What is the capital of France?"], n_results=1))
